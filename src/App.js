05fc7b482251db3d2725ab6e6d1bdd6756543a2d
Dev0109 <devtop0109@gmail.com>
2023-08-06 18:19:49 +0200

    update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="gttautopaint" element={<GetColor />}/>
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
